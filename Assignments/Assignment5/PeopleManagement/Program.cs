8319a7cfeeff418e54faafc0f3427043fb08c11d
Suneer Ranjitkar <123suneer.ranjitkar@gmail.com>
2024-08-17 23:25:32 +0545

    Add assignment 5 PeopleManagement handling .csv file

---
+++ Assignments/Assignment5/PeopleManagement/Program.cs
@@ -0,0 +1,8 @@
+﻿using PeopleManagement.Methods;
+using PeopleManagement.Model;
+
+string filePath = @"C:\Users\suneer_teksewa\OneDrive\Desktop\Dotnet Training\Github Training\OOP-fundamentals\Assignments\Assignment5\PeopleManagement\CSVFiles\";
+List<Person> people = await CSVParser.ParseCSV(filePath);
+await PeopleReport.SaveMales(people, filePath);
+await PeopleReport.SaveFemales(people, filePath);
+await PeopleReport.SaveDotComUsers(people, filePath);
